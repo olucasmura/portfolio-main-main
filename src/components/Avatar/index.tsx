d043c891fa49545c4bb4b43343f105536f89e9ba
olucasmura <152722401+olucasmura@users.noreply.github.com>
2024-11-10 22:05:30 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './Styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/olucasmura.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/olucasmura.png" />
-    <Titulo fontSize={20}>Lucas Silva</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Silva</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        olucasmura
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
